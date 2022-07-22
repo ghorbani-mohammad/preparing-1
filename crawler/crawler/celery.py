e8ce6223774ffc4d3fe7478c7163ec81f5ed643d
Mohammad <aali361@gmail.com>
2022-07-22 16:35:10 +0430

    fix: sovle celery start up issue

--- crawler/crawler/celery.py
+++ crawler/crawler/celery.py
@@ -1,10 +1,8 @@
-from __future__ import absolute_import
-
 import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTING_MODULE", "crawler.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crawler.settings")
 app = Celery("crawler")
 app.config_from_object("django.conf.settings")
 app.autodiscover_tasks(lambda:settings.INSTALLED_APPS)
