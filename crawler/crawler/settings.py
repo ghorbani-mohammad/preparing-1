f47673a0dd382c54de51e9aaa6e261b821f402d9
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-26 21:12:03 +0330

    minor change

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -72,9 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'crawler.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
